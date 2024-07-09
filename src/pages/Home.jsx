18dfa861f824c51ec1759e12b5303397230df176
abdullohn001 <muhammadaligulomov41@gmail.com>
2024-07-09 14:01:10 +0500

    register full

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+//components
+import ProductsList from "../components/ProductsList";
+
+//costun Fetch
+import { customFetch } from "../utils";
+//import react
+import { useState } from "react";
+
+//Loader
+export const loader = async () => {
+  const requset = await customFetch();
+  const products = requset.data;
+
+  return { products };
+};
+
+function Home() {
+  const [withibleProduct, ] = useState();
+  return (
+    <div className="w-full">
+      <ProductsList withibleProduct={withibleProduct} />
+      <div className="flex justify-center my-10">
+        {" "}
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
